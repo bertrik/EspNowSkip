a5caa2585118d2cc901042892d82b9eca9f8d79b
Bertrik Sikken <bertrik@sikken.nl>
2018-06-30 00:43:27 +0200

    Add skip and get function.

--- skipforwarder/skipforwarder.ino
+++ skipforwarder/skipforwarder.ino
@@ -1,8 +1,13 @@
+#include <ctype.h>
+
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
+#include <HttpClient.h>
+//#include <ESP8266WiFi.h>
+#include "ESP8266HTTPClient.h"
 
 #include "cmdproc.h"
 #include "editline.h"
@@ -14,7 +19,7 @@ static WiFiManager wifiManager;
 static WiFiClient wifiClient;
 static PubSubClient mqttClient(wifiClient);
 
-static char line[100];
+static char line[200];
 
 void setup(void)
 {
@@ -27,32 +32,105 @@ void setup(void)
     // get ESP id
     sprintf(esp_id, "%08X", ESP.getChipId());
     print("ESP ID: %s\n", esp_id);
-
-    // connect to wifi
-    print("Starting WIFI manager ...");
-    wifiManager.autoConnect("ESP-SKIP");
-    print("setup done\n");
 }
 
-static int do_jukebox(int argc, char *argv[])
+static int do_wifi(int argc, char *argv[])
 {
-    return 0;
+    if (argc > 1) {
+        char *ssid = argv[1];
+        if (argc == 2) {
+            // connect without password
+            print("Connecting to AP %s\n", ssid);
+            WiFi.begin(ssid);
+        } else if (argc == 3) {
+            // connect with password
+            char *pass = argv[2];
+            print("Connecting to AP '%s', password '%s'\n", ssid, pass);
+            WiFi.begin(ssid, pass);
+        }
+        // wait for connection
+        for (int i = 0; i < 20; i++) {
+            print(".");
+            if (WiFi.status() == WL_CONNECTED) {
+                break;
+            }
+            delay(500);
+        }
+    }
+
+    // show wifi status
+    int status = WiFi.status();
+    print("Wifi status = %d\n", status);
+
+    return (status == WL_CONNECTED) ? 0 : status;
+}
+
+static String append(const char *param, const char *value)
+{
+    String s = String(param) + "=";
+    for (const char *p = value; *p != 0; p++) {
+        char c = *p;
+        if (isalnum(c)) {
+            s += c;
+        } else {
+            char buf[8];
+            snprintf(buf, sizeof(buf), "%%%02X", c);
+            s += buf;
+        }
+    }
+    return s;
+}
+
+static String create_jukebox_url(const char *p0, const char *p1, const char *p2, const char *player)
+{
+    String url = "/Classic/status_header.html";
+    url += append("?p0", p0);
+    url += append("&p1", p1);
+    url += append("&p2", p2);
+    url += append("&player", player);
+
+    return url;
+}
+
+
+static int do_skip(int argc, char *argv[])
+{
+    String url = "http://jukebox.space.revspace.nl:9000" + 
+        create_jukebox_url("playlist", "jump", "+1", "b8:27:eb:ba:bc:d5");
+    print("url = %s\n", url.c_str());
+
+    HTTPClient httpClient;
+    httpClient.begin(url);
+    int result = httpClient.GET();
+    httpClient.end();
+
+    return result;
 }
 
 static int do_get(int argc, char *argv[])
 {
     if (argc < 2) {
-        print("Please provide an URL\n");
+        print("Please provide a URL\n");
         return CMD_ARG;
     }
-    
+
+    char *url = argv[1];
+
+    HTTPClient httpClient;
+    httpClient.begin(url);
+    int result = httpClient.GET();
+    httpClient.end();
+
+    print("Result: %d\n", result);
+
     return CMD_OK;
 }
 
 static int do_help(int argc, char *argv[]);
 
 const cmd_t commands[] = {
-    {"jukebox", do_jukebox, "Send a jukebox command"},
+    {"wifi",    do_wifi,    "<ssid> [password] connect to wifi"},
+    {"skip",    do_skip,    "Send a skip command"},
     {"get",     do_get,     "[url] Perform a HTTP GET"},
     {"help",    do_help,    "Show help"},
     {NULL, NULL, NULL}
